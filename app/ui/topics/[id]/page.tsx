da43169b2120f4eb16c579558ee445ef23ded4ea
BoostedT <tyler.whitchurch@atlasschool.com>
2025-09-29 17:46:01 -0500

    task 1

---
+++ app/ui/topics/[id]/page.tsx
@@ -0,0 +1,6 @@
+export default async function TopicPage({ params }: { params: { id: string } }) {
+  const { id } = await params;
+  await new Promise((r) => setTimeout(r, 3000));
+
+  return <div>Topic: {id}</div>;
+}
